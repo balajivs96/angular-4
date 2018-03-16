c935c0fe0db2cdffe92635369a0f687d60f135cd
balajivS <36477137+balajivs96@users.noreply.github.com>
2018-03-16 16:43:44 +0530

    Create addition.ts

---
+++ addition.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent 
+{
+  public sum: number ;
+  Add(a,b)
+  {
+    var aa=+a;
+    var bb=+b;
+    alert(this.sum= aa+bb); 
+    
+  }
+}
